b3de6e403c6114b539e4ae21d7513c1fadf46f46
angeredgecko <greg.zbo3@gmail.com>
2019-05-06 21:32:16 -0400

    commit from in school work

---
+++ InsertionSorter.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertionSorter extends Sorter{
+
+    public InsertionSorter(ArrayList< String> usersData) {
+        super(usersData);
+    }
+
+    public static <T extends Comparable<T>> void insertionSort(List<T> list) {
+        for (int currentIndex = 1; currentIndex < list.size(); currentIndex++) {
+            insert1(list, currentIndex);
+        }
+    }
+
+    private static <T extends Comparable<T>> void insert1(List<T> list, int numSorted) {
+        T valueToInsert = list.get(numSorted);
+        int indexToCompare;
+        for (indexToCompare = numSorted - 1; indexToCompare >= 0; indexToCompare--) {
+            T valueToCompare = list.get(indexToCompare);
+            if (valueToInsert.compareTo(valueToCompare) < 0) list.set(indexToCompare + 1, valueToCompare);
+            else {
+                list.set(indexToCompare + 1, valueToInsert);
+                break;
+            }
+        }
+        if (indexToCompare == -1) list.set(0, valueToInsert);
+    }
+
+    public void mySort() {
+        insertionSort(elements);
+    }
+}
---
+++ SelectionSorter.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectionSorter extends Sorter{
+
+    public SelectionSorter(ArrayList< String> usersData) {
+        super(usersData);
+    }
+
+    private static <T> void swap(List<T> challengers, int idx1, int idx2) {
+        T temp = challengers.get(idx2);
+        challengers.set(idx2, challengers.get(idx1));
+        challengers.set(idx1, temp);
+    }
+
+    private static <T extends Comparable<T>> void selectionSort(List<T> unordered) {
+        for (int i = 0; i < unordered.size(); i++) {
+            //System.out.println("Sorting: " + i + ", " + unordered);
+            swap(unordered, i, champIndex(unordered, i));
+        }
+    }
+
+    private static <T extends Comparable<T>> int champIndex(List<T> challengers, int start) {
+        T minVal = null;
+        int minIndex = 0;
+        for (int i = start; i < challengers.size(); i++) {
+            if (challengers.get(i) != null && (minVal == null || challengers.get(i).compareTo(minVal) < 0)) {
+                minVal = challengers.get(i);
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public void mySort() {
+        selectionSort(elements);
+    }
+}
