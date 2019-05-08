e0f3b3b5d318db64e9167f95c67818c637060140
Greg Z <greg.zbo3@gmail.com>
2019-05-07 22:12:07 -0400

    added more comments/description for methods

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -7,6 +7,11 @@ public class InsertionSorter extends Sorter{
         super(usersData);
     }
 
+    /**
+     * pre-conditions: unordered != null
+     * synopsis: runs insert1 for positions 1 through unordered.size()-1 in the list
+     * post-conditions: unordered will be sorted by its natural ordering
+     */
     public static <T extends Comparable<T>> void insertionSort(List<T> list) {
         for (int currentIndex = 1; currentIndex < list.size(); currentIndex++) {
             System.out.println("Sorting: " + currentIndex + ", " + list);
@@ -14,6 +19,13 @@ public class InsertionSorter extends Sorter{
         }
     }
 
+    /**
+     * pre-conditions: unordered != null
+     * synopsis: insert the first unsorted element in to the correct sorted position within
+     * the sorted region shifting values to the right to make space
+     * post-conditions: the next value will have been sorted and the sorted
+     * region will have expanded by 1
+     */
     private static <T extends Comparable<T>> void insert1(List<T> list, int numSorted) {
         T valueToInsert = list.get(numSorted);
         int indexToCompare;
--- SelectionSorter.java
+++ SelectionSorter.java
@@ -7,12 +7,22 @@ public class SelectionSorter extends Sorter{
         super(usersData);
     }
 
+    /**
+     * pre-conditions: challengers != null, idx1 and idx2 are valid indices of challenges
+     * post-conditions: the values at index idx1 and idx2 will have been swapped
+     */
     private static <T> void swap(List<T> challengers, int idx1, int idx2) {
         T temp = challengers.get(idx2);
         challengers.set(idx2, challengers.get(idx1));
         challengers.set(idx1, temp);
     }
 
+    /**
+     * pre-conditions: unordered != null
+     * synopsis: runs champIndex for positions 0 through unordered.size()-1 in the list and
+     * swaps the index from champIndex with the index currently being iterated through
+     * post-conditions: unordered will be sorted by its natural ordering
+     */
     private static <T extends Comparable<T>> void selectionSort(List<T> unordered) {
         for (int i = 0; i < unordered.size(); i++) {
             System.out.println("Sorting: " + i + ", " + unordered);
@@ -20,6 +30,11 @@ public class SelectionSorter extends Sorter{
         }
     }
 
+    /**
+     * pre-conditions: unordered != null
+     * synopsis: finds the minimum value in the unsorted portion of the list
+     * post-conditions: N/A
+     */
     private static <T extends Comparable<T>> int champIndex(List<T> challengers, int start) {
         T minVal = null;
         int minIndex = 0;
